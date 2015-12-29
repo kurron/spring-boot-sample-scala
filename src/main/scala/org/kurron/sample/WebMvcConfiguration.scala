90e177f69b9e341cb95b671601ba81eac2d03321
Ron Kurr <kurr@kurron.org>
2015-12-29 19:39:09 +0000

    Ported over the web mvc configuration to Scala.

--- src/main/scala/org/kurron/sample/WebMvcConfiguration.scala
+++ src/main/scala/org/kurron/sample/WebMvcConfiguration.scala
@@ -13,24 +13,19 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.kurron.sample
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+import org.springframework.web.servlet.config.annotation.{InterceptorRegistry, WebMvcConfigurerAdapter}
 
 /**
- * Allows us to customize Spring MVC in a Spring Boot friendly manner.
- */
+  * Allows us to customize Spring MVC in a Spring Boot friendly manner.
+  */
 @Configuration
-class WebMvcConfiguration extends WebMvcConfigurerAdapter {
-
-    @Autowired
-    CorrelationIdHandlerInterceptor correlationIdHandlerInterceptor
-
-    @Override
-    void addInterceptors( final InterceptorRegistry registry ) {
-        registry.addInterceptor( correlationIdHandlerInterceptor )
-    }
+class WebMvcConfiguration( @Autowired val interceptor: CorrelationIdHandlerInterceptor ) extends WebMvcConfigurerAdapter {
+  override def addInterceptors(registry: InterceptorRegistry): Unit = {
+    registry.addInterceptor( interceptor )
+  }
 }
