90f200ffaf22b67a3e7a781bf53d2b269d7853a3
Ron Kurr <kurr@kurron.org>
2015-12-29 17:08:42 +0000

    Moved ApplicationProperties over to Scala.

--- src/main/scala/org/kurron/sample/ApplicationProperties.scala
+++ src/main/scala/org/kurron/sample/ApplicationProperties.scala
@@ -13,18 +13,22 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.kurron.sample
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 
+import scala.beans.BooleanBeanProperty
+
 /**
- * Application specific properties. This can be injected into beans to share values.
- */
-@ConfigurationProperties( value = 'hid', ignoreUnknownFields = false )
+  * Application specific properties. This can be injected into beans to share values.
+  */
+@ConfigurationProperties( value = "hid", ignoreUnknownFields = false )
 class ApplicationProperties {
 
-    /**
-     * Flag controlling whether or not the correlation id is required.
-     */
-    boolean requireCorrelationId
+  /**
+    * Flag controlling whether or not the correlation id is required.
+    */
+  @BooleanBeanProperty
+  var requireCorrelationId: Boolean = false
 }
